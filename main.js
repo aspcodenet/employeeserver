e79001813f18422e7bedda104093056d0ca84f33
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-01-28 11:20:06 +0100

    a

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000 // "Radiofrekvens"
 
+app.use(bodyParser.json())
 app.use(cors())
 
 const employees = [{
@@ -38,6 +40,53 @@ app.get('/api/employees/:anvId',(req,res)=>{
     }
     res.json(p)
 });
+
+function getNextId(){
+    let m = Math.max(...employees.map(employee => employee.id))
+    return m + 1
+}
+
+
+app.post('/api/employees',(req,res)=>{
+    console.log(req.body)
+    var newEmp = {
+        name: req.body.name,
+        birthDate: req.body.birthDate,
+        hourlySalary: req.body.hourlySalary
+    }
+    newEmp.id = getNextId()
+    employees.push(newEmp)
+     res.status(201).json(newEmp)
+});
+
+
+app.put('/api/employees/:anvId',(req,res)=>{
+    console.log(req.params.anvId)
+    let p = employees.find(employee=>employee.id == req.params.anvId)
+    // 404???
+    if(p == undefined){
+        res.status(404).send('Finns inte')
+    }
+    p.birthDate = req.body.birthDate
+    p.hourlySalary = req.body.hourlySalary
+    p.name = req.body.name
+
+    res.status(204).send('')
+});
+
+
+
+app.delete('/api/employees/:anvId',(req,res)=>{
+    console.log(req.params.anvId)
+    let p = employees.find(employee=>employee.id == req.params.anvId)
+    // 404???
+    if(p == undefined){
+        res.status(404).send('Finns inte')
+    }
+    employees.splice(employees.indexOf(p),1)
+    res.status(204).send('')
+});
+
 
 
 
